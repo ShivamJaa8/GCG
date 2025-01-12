cf2c9472b11593c77179179ae2285041e982d919
Shivam <shivamchaudhary22224@gmail.com>
2025-01-12 08:45:02 +0530

    This is a chat group made for my College project. Here I made it by using Java,XML and Firebase as Database.I named it GCG as it stands for Galgotias Chat Group

---
+++ app/src/main/java/com/example/gcg/registration.java
@@ -0,0 +1,182 @@
+package com.example.gcg;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class registration extends AppCompatActivity {
+    TextView loginbut;
+    EditText rg_username,rg_email,rg_password,rg_repassword;
+    Button rg_signup;
+    CircleImageView rg_profileImg;
+    FirebaseAuth auth;
+    Uri imageURI;
+    String imageuri;
+    String emailpattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registration);
+        getSupportActionBar().hide();
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        loginbut = findViewById(R.id.loginbut);
+        rg_username = findViewById(R.id.rgusername);
+        rg_email = findViewById(R.id.rgemail);
+        rg_password = findViewById(R.id.rgPassword);
+        rg_repassword = findViewById(R.id.rgrePassword);
+        rg_profileImg = findViewById(R.id.profilerg);
+        rg_signup = findViewById(R.id.signupbutton);
+
+        loginbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(registration.this, login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        rg_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String namee = rg_username.getText().toString();
+                String emaill = rg_email.getText().toString();
+                String Password = rg_password.getText().toString();
+                String cpassword = rg_repassword.getText().toString();
+                String status = "Hey I am using GCG";
+
+                if (TextUtils.isEmpty(namee) || TextUtils.isEmpty(emaill) || TextUtils.isEmpty(Password) || TextUtils.isEmpty(cpassword)) {
+                    Toast.makeText(registration.this, "Please Enter valid information", Toast.LENGTH_SHORT).show();
+
+                } else if (!emaill.matches(emailpattern)) {
+                    rg_email.setError("Type a valid email here");
+
+                } else if (Password.length() < 6) {
+                    rg_password.setError("Password must contain atleast 6 character");
+                } else if (!Password.equals(cpassword)) {
+                    rg_password.setError("The Password dosen't match");
+                } else {
+                    auth.createUserWithEmailAndPassword(emaill, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                String id = task.getResult().getUser().getUid();
+                                DatabaseReference reference = database.getReference().child("user").child(id);
+                                StorageReference storageRefrence = storage.getReference().child("upload").child(id);
+
+                                if (imageURI != null) {
+                                    storageRefrence.putFile(imageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                storageRefrence.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                    @Override
+                                                    public void onSuccess(Uri uri) {
+                                                        imageuri = uri.toString();
+                                                        Users users = new Users(id, namee, emaill, Password, imageuri,status);
+                                                        reference.setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    Intent intent = new Intent(registration.this, MainActivity.class);
+                                                                    startActivity(intent);
+                                                                    finish();
+                                                                } else {
+                                                                    Toast.makeText(registration.this, "Error in creating the user", Toast.LENGTH_SHORT).show();
+                                                                }
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            }
+                                        }
+                                    });
+                                } else {
+
+                                    String status = "Hey i'm using GCG";
+                                    imageuri = "https://firebasestorage.googleapis.com/v0/b/galgotias-chat-group.appspot.com/o/man%5B1%5D.png?alt=media&token=738b7aeb-34c4-4bca-ad22-04945c22cd0c";
+                                    Users users = new Users(id, namee, emaill, Password, imageuri, status);
+                                    reference.setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Intent intent = new Intent(registration.this, MainActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            } else {
+                                                Toast.makeText(registration.this, "Error in creating the user", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                                }
+                            } else {
+                                Toast.makeText(registration.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+                }
+            }
+        });
+
+        rg_profileImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 10);
+            }
+        });
+
+    }
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if(requestCode==10){
+                if(data!=null){
+                    imageURI=data.getData();
+                    rg_profileImg.setImageURI(imageURI);
+                }
+            }
+        }
+
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+
